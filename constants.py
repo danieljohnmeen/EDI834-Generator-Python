30536a82649e90bcc7cdfb5e7c955be3a25f6f72
danieljohnmeen <danijomeen@gmail.com>
2023-07-20 09:49:29 +0200

    init constants.py and load values from it

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Define constants variables
+
+ENROLLEE_CSV = 'enrollees.csv'
+DEPENDENT_CSV = 'dependents.csv'
+PROVIDER_NAME_COL = 'Benefit Plan Provider Name'
+EMPLOYEE_ID_COL = 'EmployeeId'
+FIRST_NAME_COL = 'FirstName'
+LAST_NAME_COL = 'LastName'
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pandas as pd
+from constants import *
 
 # Load the data
-enrollees = pd.read_csv('enrollees.csv')
-dependents = pd.read_csv('dependents.csv')
+enrollees = pd.read_csv(ENROLLEE_CSV)
+dependents = pd.read_csv(DEPENDENT_CSV)
 
 # Group enrollees by benefit plan provider name
-grouped = enrollees.groupby('Benefit Plan Provider Name')
+grouped = enrollees.groupby(PROVIDER_NAME_COL)
 
 for provider_name, enrollee_group in grouped:
     print(f"Provider Name: {provider_name}")
     for _, enrollee in enrollee_group.iterrows():
-        print(f"Enrollee: {enrollee['FirstName']} {enrollee['LastName']}")
-        enrollee_dependents = dependents[dependents['EmployeeId'] == enrollee['EmployeeId']]
+        print(f"Enrollee: {enrollee[FIRST_NAME_COL]} {enrollee[LAST_NAME_COL]}")
+        enrollee_dependents = dependents[dependents[EMPLOYEE_ID_COL] == enrollee[EMPLOYEE_ID_COL]]
         for _, dependent in enrollee_dependents.iterrows():
-            print(f"\tDependent: {dependent['FirstName']} {dependent['LastName']} - {provider_name}")
+            print(f"\tDependent: {dependent[FIRST_NAME_COL]} {dependent[LAST_NAME_COL]}")
